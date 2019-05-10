165b4c881cf7c3b58aee9910b55380e8ff6fcf5e
clusk <mjm10r@my.fsu.edu>
2019-05-10 18:53:26 -0400

    homework for michael

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# FriendFinderMichael
-survey that registers different users and determines their compatibility.
+# FriendFinder
+Friend Finder: Node and Express Servers 
+
+# Description
+The Friend Finder app finds the best match from a list of users based on results from a survey.
+This data is then saved to an api then future results are compared against it to identify the best possible match.
+
---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendsArray = [
+    {
+        "name": "Kay Nguyen",
+        "photo": "twitter.com/something.jpg",
+        "scores": ["5","1","4","4","5","1","2","5","4","1"]
+    },
+    {
+        "name": "Candice Le",
+        "photo": "twitter.com/something.jpg",
+        "scores": ["4","2","5","1","3","2","2","1","3","2"]
+    },
+    {
+        "name": "Vinny Tung",
+        "photo": "twitter.com/something.jpg",
+        "scores": ["5","2","2","2","4","1","3","2","5","5"]
+    },
+    {
+        "name": "Kristen Barrett",
+        "photo": "twitter.com/something.jpg",
+        "scores": ["5","2","2","2","4","1","3","2","5","5"]
+    }
+]
+
+module.exports = friendsArray;
